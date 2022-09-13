8b530408e2d2b1d272846f25c0efe2b3e44d5def
TravisKoz <tkozlowski@student.cvtc.edu>
2022-09-13 15:09:55 -0500

    Add Database scripts

---
+++ PointOfSales/dbScripts/dbo.Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]   INT          NOT NULL,
+    [Username] VARCHAR (40) NULL,
+    [Password] VARCHAR (30) NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
