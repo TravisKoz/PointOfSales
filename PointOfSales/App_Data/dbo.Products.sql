5bbccd8e67ef05e6d3192b3592fd28c79cace48b
TravisKoz <tkozlowski@student.cvtc.edu>
2022-09-14 13:15:43 -0500

    Changed productID to UPC and make it auto incremented

---
+++ PointOfSales/App_Data/dbo.Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT           NOT NULL,
+    [Name]        VARCHAR (50)  NULL,
+    [Price]       DECIMAL (18)  NULL,
+    [Description] VARCHAR (500) NULL,
+    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
