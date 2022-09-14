4ae6ca85f1ff5d806188043728b79bb550940527
Joshua Cooley <jcooley7@cvtc.edu>
2022-09-14 13:36:19 -0500

    Setting the Background color back

--- PointOfSales/frmLogin.Designer.vb
+++ PointOfSales/frmLogin.Designer.vb
@@ -86,7 +86,7 @@ Partial Class frmLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Orange
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
         Me.ClientSize = New System.Drawing.Size(471, 290)
         Me.Controls.Add(Me.txtPassword)
         Me.Controls.Add(Me.txtUserName)
