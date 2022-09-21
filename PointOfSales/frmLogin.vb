2fc2f2bd12950d7ec363f364ca02ac1705efd051
TravisKoz <tkozlowski@student.cvtc.edu>
2022-09-20 20:00:54 -0500

    df

--- PointOfSales/frmLogin.vb
+++ PointOfSales/frmLogin.vb
@@ -11,11 +11,12 @@ Public Class frmLogin
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         Dim frmSalesWindow As New frmPointOfSale
 
-        'After the valid login is made add this, so the first form goes away.
+        'Hides the Login Form
         Me.Hide()
 
         'Navigates to the Point of Sale Form
         frmSalesWindow.ShowDialog()
+
     End Sub
 
     Private Sub LoadUsers()
