0a3d8c36bb30c39b5e04e3a817048aa6c83c8641
tquinn6 <tquinn6@student.cvtc.edu>
2022-10-09 11:07:20 -0500

    Added query for Transaction_Product table

---
+++ PointOfSales/dbScripts/dbo.Transaction_Product.sql
@@ -0,0 +1,14 @@
+﻿-- To run this query properly, make sure you right click dboProducts.mdf, 
+-- left click New Query, then paste this query into the new query
+
+-- Drop the old table if it exists
+DROP TABLE IF EXISTS [dbo].[Transaction_Product]
+
+-- Create the table Transactions
+CREATE TABLE [dbo].[Transaction_Product]
+(
+	[TransactionID] INT NOT NULL PRIMARY KEY, 
+    [UPC] INT NOT NULL, FOREIGN KEY ([UPC]) REFERENCES [dbo].[Products],
+    [Quantity] INT NULL,
+);
+
